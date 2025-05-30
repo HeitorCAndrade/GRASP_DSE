729017a19b7bea714a9c5279f19a423e23c1bfc3
HeitorCAndrade <hcandrade@inf.ufrgs.br>
2025-05-29 23:48:31 -0300

    testing paretto script

--- paretto_runs.py
+++ paretto_runs.py
@@ -14,6 +14,7 @@ def run_paretto_runs(dir_tcl):
     sol_number = 0
     for run in runs:
         print(f'run: {run}')
+        print(f'regex: {re.findall(r'\d+', run)}')
         sol_number = (re.findall(r'\d+', run))[0]
         subprocess.run(f'cp {os.path.join(cwd, dir_tcl, run)} ./directives.tcl', shell=True)
         print(f'preparing run {sol_number}...')
